ae498adca5e9483e5bbbf331e0b6e76a0ea6a4b3
FilipKDev <filip.kwasniewski@gmail.com>
2025-02-06 19:03:11 +0000

    created parent node based on HTMLNode that accepts children as argument

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,9 +1,8 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, children=None, props=None):
-        super().__init__(tag, value, children, props)
-        self.children = None
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
         if self.value == None:
---
+++ src/parentnode.py
@@ -0,0 +1,17 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("parent node must have a tag")
+        if len(self.children) == 0:
+            raise ValueError("parent node must have children")
+        html_string = ""
+        for child in self.children:
+            html_string += child.to_html()
+        return f"<{self.tag}>{html_string}</{self.tag}>"
+        
+
