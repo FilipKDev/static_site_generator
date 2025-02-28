066492e926d4623339fb9a0bf3cf3de19ce965e6
FilipKDev <filip.kwasniewski@gmail.com>
2025-02-28 19:34:52 +0000

    1. Created smarter markdown_to_blocks function that properly seperates content inside code blocks without messing with its formatting 2. Created a recursive function to generate multiple pages from markdown files maintaining its folder structure 3. Minor bug fixes

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -10,16 +10,43 @@ def extract_title(markdown):
     else:
         raise Exception("No header found")
         
-
 def markdown_to_blocks(markdown):
     markdown_split = markdown.split("\n\n")
     string_list = []
-    for string in markdown_split:
+    for string in markdown_split: 
         if string != "":
             string = string.strip("\n")
             string = string.strip()
             string_list.append(string)
     return string_list
+
+def markdown_to_blocks_smart(markdown):
+    markdown_split = markdown.split("\n\n")
+    code_contents = check_for_code_blocks(markdown)
+    code_contents_index = 0
+    inside_code_block = False
+    string_list = []
+    for i in range(0, len(markdown_split)):
+        string = markdown_split[i]
+        if string != "" and string != "```" and not inside_code_block:
+            string = string.strip("\n")
+            string = string.strip()
+            string_list.append(string)
+        elif string == "```" and not inside_code_block:
+            inside_code_block = True
+            string_list.append("```" + code_contents[code_contents_index] + "\n```")
+        elif string == "```" and inside_code_block:
+            inside_code_block = False
+            code_contents_index += 1
+    return string_list
+
+def check_for_code_blocks(markdown):
+    split_by_code_lines = markdown.split("\n```")
+    code_contents = []
+    for i in range(0, len(split_by_code_lines)):
+        if i % 2 == 1 and i < len(split_by_code_lines) - 1:
+            code_contents.append(split_by_code_lines[i])
+    return code_contents
 
 def block_to_block_type(markdown):
     if re.match(r"#{1,6}(?= )", markdown):
@@ -36,7 +63,7 @@ def block_to_block_type(markdown):
         return "normal"
 
 def markdown_to_html_node(markdown):
-    split_document = markdown_to_blocks(markdown)
+    split_document = markdown_to_blocks_smart(markdown)
     block_nodes = []
     for block in split_document:
         block_type = block_to_block_type(block)
--- src/main.py
+++ src/main.py
@@ -11,13 +11,25 @@ def copy_files(source_path, destination_path):
         new_source_path = os.path.join(source_path, item)
         new_destination_path = os.path.join(destination_path, item)
         if os.path.isfile(new_source_path):
-            print(f"Copying {item} from {source_path} to {destination_path}")
+            print(f"\nCopying {item} from {source_path} to {destination_path}")
             shutil.copy(new_source_path, new_destination_path)
         else:
-            print(f"looking in {new_source_path}")
+            print(f"\nlooking in {new_source_path}")
             if not os.path.exists(new_destination_path):
                 os.mkdir(new_destination_path)
             copy_files(new_source_path, new_destination_path)
+    return
+
+def generate_multiple_pages(from_path, template_path, dest_path):
+    for item in os.listdir(from_path):
+        new_from_path = os.path.join(from_path, item)
+        new_dest_path = os.path.join(dest_path, item)
+        if os.path.isfile(new_from_path):
+            print(f"\nGenerating html page from {new_from_path}")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            print(f"\nlooking for markdown in {new_from_path}")
+            generate_multiple_pages(new_from_path, template_path, new_dest_path)
     return
 
 def generate_page(from_path, template_path, dest_path):
@@ -29,12 +41,11 @@ def generate_page(from_path, template_path, dest_path):
 
     template = read_template(template_path)
     webpage = template.replace("{{ Title }}", title)
-    #print(f"\n{markdown_html_node.to_html()}")
     webpage = webpage.replace("{{ Content }}", markdown_html_node.to_html())
 
     if not os.path.exists(dest_path):
-        os.makedirs(dest_path)
-    webpage_file = open(os.path.join(dest_path, "index.html"), "x")
+        os.makedirs(dest_path, exist_ok=True)
+    webpage_file = open(os.path.join(dest_path, "index.html"), "w")
     webpage_file.write(webpage)
     webpage_file.close()
 
@@ -43,16 +54,20 @@ def read_markdown(from_path):
         raise Exception("Invalid path")
     for file in os.listdir(from_path):
         if os.path.isfile(os.path.join(from_path, file)) and file[-3:] == ".md":
-            markdown = open(os.path.join(from_path, file)).read()
-            return markdown
+            markdown_file = open(os.path.join(from_path, file))
+            markdown_content = markdown_file.read()
+            markdown_file.close()
+            return markdown_content
 
 def read_template(template_path):
     if not os.path.exists(template_path):
         raise Exception("Invalid path")
     for file in os.listdir(template_path):
         if os.path.isfile(os.path.join(template_path, file)) and file[-5:] == ".html":
-            template = open(os.path.join(template_path, file)).read()
-            return template
+            template_file = open(os.path.join(template_path, file))
+            template_content = template_file.read()
+            template_file.close()
+            return template_content
 
 def main():
     from_path = "/home/filip/workspace/github.com/FilipKDev/static_site_generator/content"
@@ -62,7 +77,7 @@ def main():
     if os.path.exists(dest_path):
         print(f"removing {dest_path} and its contents")
         shutil.rmtree(dest_path)
-    generate_page(from_path, template_path, dest_path)
     copy_files("/home/filip/workspace/github.com/FilipKDev/static_site_generator/static", dest_path)
+    generate_multiple_pages(from_path, template_path, dest_path)
 
 main()
